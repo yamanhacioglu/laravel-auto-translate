e73695799f69d3e9c8b879dc2a1fc5e4d45f8406
Yaman HACIOĞLU <59294991+yamanhacioglu@users.noreply.github.com>
2024-10-12 01:50:31 +0300

    Create AutoTranslateServiceProvider.php
    AutoTranslateServiceProvider created.

---
+++ src/AutoTranslateServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Northlab\AutoTranslate;
+
+use Illuminate\Support\ServiceProvider;
+use Northlab\AutoTranslate\Services\TranslationService;
+
+class AutoTranslateServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Publish configuration
+        $this->publishes([
+            __DIR__.'/../config/auto-translate.php' => config_path('auto-translate.php'),
+        ], 'config');
+
+        // Publish migrations
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_api_configs_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_api_configs_table.php'),
+        ], 'migrations');
+    }
+
+    public function register()
+    {
+        // Merge configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/auto-translate.php', 'auto-translate');
+
+        // Register the main service
+        $this->app->singleton(TranslationService::class, function ($app) {
+            return new TranslationService();
+        });
+
+        // Register the facade
+        $this->app->bind('auto-translate', function($app) {
+            return $app->make(TranslationService::class);
+        });
+    }
+}
